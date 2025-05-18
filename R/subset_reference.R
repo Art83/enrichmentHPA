c75a2bc613e778a5f598f408d28d30c52b4cc071
Art83 <shvetsov1983@gmail.com>
2025-05-18 16:01:36 +1000

    removed redundant arguments from subset_reference

--- R/subset_reference.R
+++ R/subset_reference.R
@@ -5,48 +5,26 @@
 #' It supports flexible column selection to adapt across different dataset types.
 #'
 #' @param data A data.frame returned by `load_reference()`.
-#' @param genes Character vector of gene names (matching `Gene.name` column).
-#' @param groups Character vector of group values (e.g., cell types, regions).
-#' @param group_col Name of the grouping column (e.g. `Immune.cell`).
-#'
 #' @return A filtered data.frame containing only matching genes and groups.
 #' @export
-subset_reference <- function(data,
-                             genes,
-                             groups,
-                             group_col,
-                             ...) {
-  # Input validation
+subset_reference <- function(data, ...) {
   if (!"Gene.name" %in% colnames(data)) {
     stop("The input data must contain a 'Gene.name' column.")
   }
-  if (!group_col %in% colnames(data)) {
-    stop("Column '", group_col, "' not found in the input data.")
-  }
-  dots <- rlang::enquos(...)
 
+  dots <- rlang::enquos(...)
   named_dots <- dots[nzchar(names(dots))]
   unnamed_dots <- dots[!nzchar(names(dots))]
 
-  # Convert named args into proper == expressions
   named_filters <- purrr::imap(named_dots, function(val, name) {
     rlang::new_quosure(
-      rlang::expr(.data[[!!name]] == !!val),
+      rlang::expr(.data[[!!name]] %in% !!val),
       env = rlang::caller_env()
     )
   })
 
-  # Combine and forcibly remove all names to avoid dplyr::filter() complaint
   all_filters <- unname(c(unnamed_dots, named_filters))
 
-
-  filtered <- dplyr::filter(
-    data,
-    Gene.name %in% genes,
-    .data[[group_col]] %in% groups,
-    !!!all_filters
-  )
-
-  return(filtered)
+  dplyr::filter(data, !!!all_filters)
 }
 

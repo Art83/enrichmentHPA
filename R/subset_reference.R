34820b0ff5241d7c50caeb3b059263238c6c33c7
Art83 <shvetsov1983@gmail.com>
2025-05-18 10:51:29 +1000

    modify subset_reference to handle multiple filter statements

--- R/subset_reference.R
+++ R/subset_reference.R
@@ -14,7 +14,8 @@
 subset_reference <- function(data,
                              genes,
                              groups,
-                             group_col) {
+                             group_col,
+                             ...) {
   # Input validation
   if (!"Gene.name" %in% colnames(data)) {
     stop("The input data must contain a 'Gene.name' column.")
@@ -22,13 +23,30 @@ subset_reference <- function(data,
   if (!group_col %in% colnames(data)) {
     stop("Column '", group_col, "' not found in the input data.")
   }
+  dots <- rlang::enquos(...)
 
-  # Filter the data
+  named_dots <- dots[nzchar(names(dots))]
+  unnamed_dots <- dots[!nzchar(names(dots))]
+
+  # Convert named args into proper == expressions
+  named_filters <- purrr::imap(named_dots, function(val, name) {
+    rlang::new_quosure(
+      rlang::expr(.data[[!!name]] == !!val),
+      env = rlang::caller_env()
+    )
+  })
+
+  # Combine and forcibly remove all names to avoid dplyr::filter() complaint
+  all_filters <- unname(c(unnamed_dots, named_filters))
+
+
   filtered <- dplyr::filter(
     data,
     Gene.name %in% genes,
-    .data[[group_col]] %in% groups
+    .data[[group_col]] %in% groups,
+    !!!all_filters
   )
 
   return(filtered)
 }
+
